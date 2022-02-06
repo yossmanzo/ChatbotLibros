1e51402898e64393122f84ff5680e0d26a6b9d67
yossmanzo <76918557+yossmanzo@users.noreply.github.com>
2022-02-06 12:37:55 -0600

    Add files via upload

---
+++ librosBot/plugins/generos.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 15 22:26:38 2022
+
+@author: lenovo
+"""
+
+nombresGeneros = ["terror", "fantasia", "suspenso"]
+
+def generosLiterarios():
+   for i in range (0,len(nombresGeneros)):
+       print("\t"+str(i+1)+".- "+nombresGeneros[i])
+    
+
+            
+          
